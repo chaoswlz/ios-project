acd99bb62ca6fcf406f1213581f58110a98fe841
Manjot Khalsa <manjotkhalsa.96@gmail.com>
2016-10-14 12:45:45 -0700

    adding single-view application code

---
+++ ios-project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-project
+//
+//  Created by Manjot Khalsa on 2016-10-14.
+//  Copyright © 2016 Manjot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ios-projectTests/ios_projectTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_projectTests.swift
+//  ios-projectTests
+//
+//  Created by Manjot Khalsa on 2016-10-14.
+//  Copyright © 2016 Manjot. All rights reserved.
+//
+
+import XCTest
+@testable import ios_project
+
+class ios_projectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

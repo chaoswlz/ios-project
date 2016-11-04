79a0461c9ef80cc04b2a23cd9bcc6fbf210eb41a
Jason Cheung <jayson.h.cheung@gmail.com>
2016-11-04 12:38:27 -0700

    GameViewController with map view and bounds

--- ios-project/Map.swift
+++ ios-project/Map.swift
@@ -17,29 +17,40 @@ class Map {
     var bottomRightPoint: MKMapPoint!
     var mapSize: MKMapSize!
     var mapActual: MKMapRect!
+    var mapRegion: MKCoordinateRegion?
     static var map : Map?
     
     //not singleton yet
+    // TODO Rename parameters to corner1, corner2
     init(topCorner: MKMapPoint, botCorner: MKMapPoint, tileSize: Int){
-        topLeftPoint = topCorner
-        bottomRightPoint = botCorner
+        // Reformat points if they are invalid.
+        let topLeftX = (topCorner.x < botCorner.x) ? topCorner.x : botCorner.x
+        let topLeftY = (topCorner.y < botCorner.y) ? topCorner.y : botCorner.y
+        let botRightX = (topCorner.x > botCorner.x) ? topCorner.x : botCorner.x
+        let botRightY = (topCorner.y > botCorner.y) ? topCorner.y : botCorner.y
+        
+        let tlp = MKMapPointMake(topLeftX, topLeftY)
+        let brp = MKMapPointMake(botRightX, botRightY)
+        
+        topLeftPoint = tlp
+        bottomRightPoint = brp
+        
         Tile.setTileSize(size: tileSize) //set default value?
         updateMapDimensions()
     }
     
     func updateMapDimensions() {
-        let tileSize : Int = Tile.getTileSize()
+        //let tileSize : Int = Tile.getTileSize()
         self.mapSizeX  = bottomRightPoint.x - topLeftPoint.x
         self.mapSizeY = bottomRightPoint.y - topLeftPoint.y
-        let xTiles = Int(floor(Double(Int(mapSizeX) / tileSize)))
-        let yTiles = Int(floor(Double(Int(mapSizeY) / tileSize)))
+        //let xTiles = Int(floor(Double(Int(mapSizeX) / tileSize)))
+        //let yTiles = Int(floor(Double(Int(mapSizeY) / tileSize)))
         
-        mapSize = MKMapSize( width: Double(Tile.getTileSize() * xTiles),
-                             height: Double(Tile.getTileSize() * yTiles))
+        mapSize = MKMapSize( width: self.mapSizeX, height: self.mapSizeY)
         
         mapActual = MKMapRect(origin: topLeftPoint, size: mapSize!)
         
-        initTiles(numTiles: xTiles * yTiles)
+        // initTiles(numTiles: abs(xTiles * yTiles))
     }
     
     func initTiles(numTiles: Int) {
